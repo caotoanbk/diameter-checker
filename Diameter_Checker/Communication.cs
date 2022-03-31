1710b866e39b9d0ec3a60687d1287d5846d3d253
toan3303 <toan3303@robotech.com.vn>
2022-03-31 16:24:05 +0700

    [update] update 220331

--- Diameter_Checker/Communication.cs
+++ Diameter_Checker/Communication.cs
@@ -166,7 +166,7 @@ namespace Diameter_Checker
             Communication.A1RecevingData = false;
             Communication.A2RecevingData = false;
             Communication.loginUser = "Admin";
-            Communication.processorIDAdmin = "BFEBFBFF000406E3";
+            Communication.processorIDAdmin = "BFEBFBFF000306C3";
             Communication.processorID = "BFEBFBFF000906E9";
             Communication.A1Detected = false;
             Communication.A2Detected = false;
